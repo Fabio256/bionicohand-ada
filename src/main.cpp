13713b884175cb088b12e7aafdf3340a5d18b39c
Constantin Clauzel <stantin04@gmail.com>
2016-03-12 14:30:54 +0100

    first commit, CmdMessenger and FingerLib

---
+++ src/main.cpp
@@ -0,0 +1,57 @@
+#include <Arduino.h>
+
+#include <CmdMessenger.h>
+
+#include "finger.h"
+
+#define MIN_SPEED 190.0f
+#define MAX_SPEED 250.0f
+
+#define MIN_POS 50.0f
+#define MAX_POS 970.0f
+
+CmdMessenger cmdMessenger = CmdMessenger(MYSERIAL);
+
+enum
+{
+  kSetFingers,
+};
+
+void OnSetFingers()
+{
+  MYSERIAL.println("OnSetFingers");
+  int i;
+  for(i = 0; i < NUM_FINGERS; ++i)
+  {
+    double pos = cmdMessenger.readDoubleArg();
+    pos = min(1, max(0, pos)) * (MAX_POS-MIN_POS) + MIN_POS;
+    double speed = cmdMessenger.readDoubleArg();
+    speed = min(1, max(0, speed)) * (MAX_SPEED-MIN_SPEED) + MIN_SPEED;
+
+    MYSERIAL.print("finger: ");
+    MYSERIAL.println(i);
+    MYSERIAL.print("pos: ");
+    MYSERIAL.println(pos);
+    MYSERIAL.print("speed: ");
+    MYSERIAL.println(speed);
+    finger[i].writeSpeed(speed);
+    finger[i].writePos(pos);
+  }
+}
+
+void setup()
+{
+  MYSERIAL.begin(115200);
+  MYSERIAL.println("bionicohand v1.0");
+
+  pinAssignment(LEFT);
+
+  cmdMessenger.printLfCr();
+
+  cmdMessenger.attach(kSetFingers, OnSetFingers);
+}
+
+void loop()
+{
+  cmdMessenger.feedinSerialData();
+}
